801f0fa97353218ff007e1d1587b22c5e9e3ed77
Martin Espinach <martin.espinach@jamgo.es>
2018-02-15 12:15:13 +0100

    Fixed bad hardcoded user name.

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-echo $VPN_PASSWORD | openconnect -q --cookieonly $OPENCONNECT_OPTIONS --disable-ipv6 -c /root/$VPN_USER.pem --protocol=nc --os=linux $VPN_URL -u B690245 --passwd-on-stdin | openconnect $OPENCONNECT_OPTIONS -b --disable-ipv6 -c /root/$VPN_USER.pem --protocol=nc --os=linux $VPN_URL --cookie-on-stdin
+echo $VPN_PASSWORD | openconnect -q --cookieonly $OPENCONNECT_OPTIONS --disable-ipv6 -c /root/$VPN_USER.pem --protocol=nc --os=linux $VPN_URL -u $VPN_USER --passwd-on-stdin | openconnect $OPENCONNECT_OPTIONS -b --disable-ipv6 -c /root/$VPN_USER.pem --protocol=nc --os=linux $VPN_URL --cookie-on-stdin
 
 iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -j ACCEPT
